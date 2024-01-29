5f2e08a2ac913fd432ce517fd05c32e38b29cf23
ved-thakre <vedantthakre7@gmail.com>
2024-01-29 22:36:06 +0530

    Register API integrated

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,14 +1,20 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/db/db";
+import { User } from "@/models/userModel";
 
 export async function POST(req){
     try {
         const { name, email, password } = await req.json();
         await connectDB();
-        console.log(name, email, password);
+        const newUser = await User.create({
+            name,
+            email,
+            password,
+        });
         return NextResponse.json({
             status: true,
             message: `Hello ${name}`,
+            data: newUser
         },
         {
             status: 201,
---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
